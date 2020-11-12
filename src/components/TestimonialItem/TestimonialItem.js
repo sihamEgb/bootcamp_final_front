e85e84221aab3592140f087fbad283b169af6cc5
sihamEgb <siham.egb@gmail.com>
2020-11-12 05:03:42 +0200

    updates

--- src/components/TestimonialItem/TestimonialItem.js
+++ src/components/TestimonialItem/TestimonialItem.js
@@ -1,19 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "./ContactUs.css";
+import "./TestimonialItem.css";
 
 const TestimonialItem = (props) => {
-  return (
-    <div className='ui'>
-      <div className='contact-us'>
-       <p className="testimonial-item__quote">r offer multiple blocks: intros, sliders, galleries, forms, articles, and so on. Start a project and click on the red plus buttons to see the blocks available for your theme.
-			 </p>
-			 <div className="testimonial-item__user">
-				 <div className="testimonial-item__user_image">avatar image</div>
-				 <div className="testimonial-item__user_name">name</div>
-			 </div>
-    </div>
-  );
+  return <div className='container__testimonialItem'>TestimonialItem Comp</div>;
 };
 
 export default TestimonialItem;
