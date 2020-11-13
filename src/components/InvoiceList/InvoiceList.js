4409331c23a935e8ed4b5a4f9f341d2b57e1364e
sihamEgb <siham.egb@gmail.com>
2020-11-13 05:05:46 +0200

    updates

---
+++ src/components/InvoiceList/InvoiceList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./InvoiceList.css";
+
+const InvoiceList = (props) => {
+  return <div className='container__invoiceList'>InvoiceList Component</div>;
+};
+
+export default InvoiceList;
