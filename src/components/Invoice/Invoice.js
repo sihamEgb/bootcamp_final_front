b3d37ac1b50d90043a577853d8f1bc3db616c863
sihamEgb <siham.egb@gmail.com>
2020-11-19 05:06:36 +0200

    updates

---
+++ src/components/Invoice/Invoice.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Invoice.css";
+
+const Invoice = (props) => {
+  return <div className='container__invoice'>Invoice Component</div>;
+};
+
+export default Invoice;
