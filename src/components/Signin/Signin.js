e3c37c73c42ffe58561c51ed3e025b957bc83f3c
sihamEgb <siham.egb@gmail.com>
2020-11-24 04:58:09 +0200

    updates

---
+++ src/components/Signin/Signin.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Sign.css";
+
+const Testimonials = (props) => {
+  return <div className='container__signIn'>Sign in Component</div>;
+};
+
+export default Testimonials;
