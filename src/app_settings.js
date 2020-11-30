af1aea84d248400cf6d16a4f5d0b0af10c2b5941
sihamEgb <siham.egb@gmail.com>
2020-11-30 16:26:26 +0200

    updates

--- src/app_settings.js
+++ src/app_settings.js
@@ -1,8 +1,7 @@
 app_name = "Invoice Digitization App";
 app_moto = "Invoice Digitization App";
 app_explanatory = "Invoice Digitization App";
-app_benefits;
-app_benefit_1 = "";
-app_benefit_1 = "";
-app_benefit_1 = "";
-app_benefit_1 = "";
+app_benefits = [];
+app_benefit_1 = "save time";
+app_benefit_2 = "save paper";
+app_benefit_3 = "save money";
